4e6c6d16ab340ab60e868e4e0d833ce161b7aeff
CodeMaverick2 <tejas.23bcs10056@ms.sst.scaler.com>
2025-09-08 17:46:30 +0530

    decorator beverages

--- beverages_decorator/Client.java
+++ beverages_decorator/Client.java
@@ -4,13 +4,13 @@ public class Client {
 
 	public static void main(String[] args) {
 		
-
-		
 		Beverage coffee = new Cappuccino();
         System.out.println("Coffee");
 		System.out.println(coffee.cost());
-		
-		
+
+		coffee = Factory.addMilk(coffee);
+		System.out.println("Milk added to Coffee");
+		System.out.println(coffee.cost());
 
 	}
 
---
+++ beverages_decorator/Factory.java
@@ -0,0 +1,8 @@
+package beverages_decorator;
+
+public class Factory {
+    public static Beverage addMilk(Beverage beverage){
+        return new MilkDecorator(beverage);
+    }  
+      
+}
